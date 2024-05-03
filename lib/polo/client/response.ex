4581cda1d088048d76d228e2a62587c6820be58d
Marcelo Santos <immarcelosantos@gmail.com>
2024-05-02 22:25:11 -0300

    docs: @type and @spec for Request

--- lib/polo/client/response.ex
+++ lib/polo/client/response.ex
@@ -2,6 +2,13 @@ defmodule Polo.Client.Response do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @type t :: %__MODULE__{
+    body: binary(),
+    status: integer(),
+    received_at: NaiveDateTime.t(),
+    headers: list(Polo.Client.Header.t())
+  }
+
   embedded_schema do
     field :body, :binary
     field :status, :integer
@@ -10,6 +17,7 @@ defmodule Polo.Client.Response do
     embeds_many :headers, Polo.Client.Header
   end
 
+  @spec changeset(__MODULE__.t, %{optional(any) => any}) :: Ecto.Changeset.t()
   def changeset(response, attrs \\ %{}) do
     response
     |> cast(attrs, [:body, :status])
