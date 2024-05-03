9609a41bbb0ca5e083bf77f3b9d20db0187d206c
Marcelo Santos <immarcelosantos@gmail.com>
2024-05-02 22:21:23 -0300

    docs: @type and @spec for Request

--- lib/polo/client/request.ex
+++ lib/polo/client/request.ex
@@ -2,6 +2,15 @@ defmodule Polo.Client.Request do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @type t :: %__MODULE__{
+    method: atom(),
+    url: String.t(),
+    sent_at: NaiveDateTime.t(),
+    headers: list(Polo.Client.Header.t()),
+    parameters: list(Polo.Client.Parameter.t()),
+    body: Polo.Client.Body.t()
+  }
+
   @valid_methods [:get, :post, :put, :patch, :delete, :options, :head]
 
   embedded_schema do
@@ -14,6 +23,7 @@ defmodule Polo.Client.Request do
     embeds_one :body, Polo.Client.Body, on_replace: :delete
   end
 
+  @spec changeset(__MODULE__.t, %{optional(any) => any}) :: Ecto.Changeset.t()
   def changeset(request, attrs \\ %{}) do
     request
     |> cast(attrs, [:method, :url])
@@ -26,6 +36,7 @@ defmodule Polo.Client.Request do
     |> maybe_add_default_parameter()
   end
 
+  @spec methods() :: list(atom())
   def methods, do: Ecto.Enum.values(__MODULE__, :method)
 
   @doc """
@@ -34,24 +45,29 @@ defmodule Polo.Client.Request do
   If we don't reset them, their error messages appear after the first submit only. Additional attempts to submit a 
   request with invalid params don't work, and no feedback is displayed.
   """
+  @spec reset_embed(Ecto.Changeset.t(), atom()) :: Ecto.Changeset.t()
   def reset_embed(changeset, embed) do
     update_change(changeset, embed, fn change ->
       %{change | action: nil, errors: []}
     end)
   end
 
+  @spec cast_headers(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp cast_headers(changeset) do
     cast_embed(changeset, :headers, sort_param: :headers_sort, drop_param: :headers_drop)
   end
 
+  @spec cast_parameters(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp cast_parameters(changeset) do
     cast_embed(changeset, :parameters, sort_param: :parameters_sort, drop_param: :parameters_drop)
   end
 
+  @spec cast_body(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp cast_body(changeset) do
     cast_embed(changeset, :body)
   end
 
+  @spec maybe_add_default_header(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp maybe_add_default_header(changeset) do
     case Ecto.Changeset.get_change(changeset, :headers) do
       nil ->
@@ -62,6 +78,7 @@ defmodule Polo.Client.Request do
     end
   end
 
+  @spec maybe_add_default_parameter(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp maybe_add_default_parameter(changeset) do
     case Ecto.Changeset.get_change(changeset, :parameters) do
       nil ->
