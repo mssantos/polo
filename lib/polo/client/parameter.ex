02fb75a192ec24d31db6f1ca17189aa3f7ead6cd
Marcelo Santos <immarcelosantos@gmail.com>
2024-05-02 22:28:09 -0300

    docs: @type and @spec for Parameter

--- lib/polo/client/parameter.ex
+++ lib/polo/client/parameter.ex
@@ -2,12 +2,18 @@ defmodule Polo.Client.Parameter do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @type t :: %__MODULE__{
+    name: String.t(),
+    value: String.t()
+  }
+
   @primary_key false
   embedded_schema do
     field :name, :string
     field :value, :string, redact: true
   end
 
+  @spec changeset(__MODULE__.t, %{optional(any) => any}) :: Ecto.Changeset.t()
   def changeset(%Polo.Client.Parameter{} = parameter, attrs) do
     cast(parameter, attrs, [:name, :value])
   end
