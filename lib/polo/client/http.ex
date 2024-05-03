dfdc77d4747ed149fc4339c209859868d2b5556a
Marcelo Santos <immarcelosantos@gmail.com>
2024-05-02 22:30:36 -0300

    docs: @spec for HTTP behaviour

--- lib/polo/client/http.ex
+++ lib/polo/client/http.ex
@@ -1,8 +1,8 @@
 defmodule Polo.Client.HTTP do
   alias Polo.Client.Request
 
-  @callback send_request(request :: %Request{}) ::
-              {:ok, response :: map()} | {:error, message :: term}
+  @callback send_request(request :: Request.t()) ::
+              {:ok, response :: map()} | {:error, message :: atom() | String.t()}
 
   def load_client do
     Application.get_env(:polo, :http_client)
